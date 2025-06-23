06d8406e7282e50a65b3e53584d11e25630597e2
rayakhader <rayakhader20@gmail.com>
2025-06-23 19:38:28 +0300

    edit the input actions

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,32 +1,34 @@
-import  { useState, type Dispatch, type SetStateAction } from 'react'
+import { useState, type Dispatch, type SetStateAction } from 'react'
 import type { Msg } from './Chat'
 type SearchBarProps = {
-    messages:Msg[], 
+    messages: Msg[],
     setMessages: Dispatch<SetStateAction<Msg[]>>
 }
 
-function SearchBar({messages,setMessages}:SearchBarProps) {
-    const[prompt, setPrompt] = useState('')
-    function handleSend(){
-        if(!prompt.trim()) alert("Prompt can't be empty")
-        setPrompt('')
-        setMessages([...messages,{
-            content:prompt
-        }])
+function SearchBar({ messages, setMessages }: SearchBarProps) {
+    const [prompt, setPrompt] = useState('')
+    function handleSend() {
+        if (!prompt.trim()) alert("Prompt can't be empty")
+        else {
+            setPrompt('')
+            setMessages([...messages, {
+                content: prompt
+            }])
+        }
     }
-    function handleKeyDown(event:React.KeyboardEvent<HTMLInputElement>){
-        if(event.key ==='Enter'){
+    function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+        if (event.key === 'Enter') {
             handleSend()
         }
     }
-  return (
-    <div className='search-bar'>
-        <input className='search-input' type='text' placeholder='Please ask what you want...' value={prompt} onChange={(e)=>setPrompt(e.target.value)} onKeyDown={handleKeyDown} />
-        <button className='send-btn' onClick={handleSend}>
-            Send
-        </button>
-    </div>
-  )
+    return (
+        <div className='search-bar'>
+            <input className='search-input' type='text' placeholder='Please ask what you want...' value={prompt} onChange={(e) => setPrompt(e.target.value)} onKeyDown={handleKeyDown} />
+            <button className='send-btn' onClick={handleSend}>
+                Send
+            </button>
+        </div>
+    )
 }
 
 export default SearchBar
